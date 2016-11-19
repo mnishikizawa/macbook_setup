228cfc464e8a05f667b0394355e1be835a4125b1
nishiki <nishiki@MacBook-Pro.local>
2016-11-19 15:57:01 +0900

    run ansible version 2.0

--- setup02.sh
+++ setup02.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+brew install pyenv-virtualenv
+pyenv install 2.7.10
+pyenv virtualenv 2.7.10 ansible2.0
+pyenv global ansible2.0
+pip install ansible==2.0
+
 ansible-playbook localhost.yml
